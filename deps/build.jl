c5925c0cb994e5a2dedda959a13dc26763bf3df7
Zenna Tavares <zennatavares@gmail.com>
2017-10-01 20:14:38 -0500

    fix url

--- deps/build.jl
+++ deps/build.jl
@@ -1,16 +1,13 @@
 version = "1.7.4"
 file_name = "sketch-$version.tar.gz"
-file_url = "https://people.csail.mit.edu/asolar/sketch-1.7.4.tar.gz/$file_name"
+file_url = "https://people.csail.mit.edu/asolar/$file_name"
 deps_dir = dirname(@__FILE__)
 prefix = joinpath(deps_dir, "usr")
 
 println("Cleanup old build files")
 
 try
-  run(`rm  -r $(joinpath(deps_dir,"dReal*"))`)
-catch end
-try
-  run(`rm -r $(joinpath(deps_dir,"usr"))`)
+  run(`rm  -r $(joinpath(deps_dir,"sketch*"))`)
 catch end
 
 download(file_url,joinpath(deps_dir, file_name))
